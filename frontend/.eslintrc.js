e889aa2636ac88fd53b3521cd14f9f81af9277d9
Xiaoyu <xiaoyuyang48@gmail.com>
2025-09-29 17:51:14 +1000

    Windows version

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
   ],
   rules: {
     'indent': ['warn', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     'quotes': ['warn', 'single'],
     'semi': ['error', 'always'],
     'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
